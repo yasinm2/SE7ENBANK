0105e9d4f2ce0843a25e27e867989386ed31b402
Ahmet Yasin Ağbulut <yasinagbulut0@gmail.com>
2023-09-04 23:11:55 +0300

    Initial Commit

---
+++ SE7ENBANK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SE7ENBANK
+//
+//  Created by Yasin Ağbulut on 4.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
